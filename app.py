6422e90d1596083133379923a28845b502900f1c
Teoh TeikToe <ttt@mensa.org.sg>
2023-06-11 09:23:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask,render_template,request
 import replicate,os
 app = Flask(__name__)
 os.environ["REPLICATE_API_TOKEN"] = "787f515cb0624813736c11e7fefec66473394f02"
-model = replicate.models.get("tstramer/midjourney-diffusion")
-version = model.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
+model=replicate.models.get("tstramer/midjourney-diffusion")
+version=model.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
